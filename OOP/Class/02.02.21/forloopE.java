bbc28af97865ef2de7ecd045d77bf26037624b5e
BantuD <500069424@stu.upes.ac.in>
2021-02-11 00:05:52 +0530

    OOP class work till 10-02-21

---
+++ OOP/Class/02.02.21/forloopE.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+public class forloopEŚ
+{
+ public static void main(String arg[])
+  {
+    char[] ch={'a','b','c','d','e'};
+    for(char x: ch)
+    {
+      System.out.println(x);
+    }
+   }
+}
